c1e5a16a314364e032a8901df6ca3589e813c101
Aleksa Milosevic <aleksa.milosevic807@gmail.com>
2025-11-11 01:48:22 +0100

    Added functionality for drawing lines, rectangles and circles in red, green, blue and white color. Added keyboard shortcuts for picking shapes and colors

---
+++ main.js
@@ -0,0 +1,195 @@
+const canvas = document.getElementById("canvas");
+
+if (canvas.getContext) {
+  // canvas is supported - rendering code
+
+  const state = {
+    mouseX: 0,
+    mouseY: 0,
+    pencilLineWidth: 1.0,
+    brushLineWidth: 1.0,
+    shapeLineWidth: 1.0,
+    mouseClicked: false,
+    pencilSelected: true,
+    brushSelected: false,
+    rectangleSelected: false,
+    circleSelected: false,
+
+    // starting coordinates for drawing shapes (rectangle, circle...)
+    shapeStartX: 0,
+    shapeStartY: 0,
+    contextImageData: null,
+  };
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const context = canvas.getContext("2d", { willReadFrequently: true });
+
+  context.strokeStyle = "rgb(255 255 255)";
+  context.lineWidth = state.pencilLineWidth;
+  window.onkeydown = (e) => {
+    switch (e.key) {
+      case "r":
+        context.strokeStyle = "rgb(255 0 0)";
+        break;
+      case "g":
+        context.strokeStyle = "rgb(0 255 0)";
+        break;
+      case "b":
+        context.strokeStyle = "rgb(0 0 255)";
+        break;
+      case "w":
+        context.strokeStyle = "rgb(255 255 255)";
+        break;
+      case "+":
+        if (state.pencilSelected && state.pencilLineWidth < 248.0) {
+          state.pencilLineWidth += 1.0;
+          context.lineWidth = state.pencilLineWidth;
+        } else if (state.brushSelected && state.brushLineWidth < 248.0) {
+          state.brushLineWidth += 1.0;
+          context.lineWidth = state.brushLineWidth;
+        } else if (state.shapeLineWidth < 20.0) {
+          state.shapeLineWidth += 1.0;
+          context.lineWidth = state.shapeLineWidth;
+        }
+        break;
+      case "-":
+        if (state.pencilSelected && state.pencilLineWidth > 1.0) {
+          state.pencilLineWidth -= 1.0;
+        } else if (state.brushSelected && state.brushLineWidth > 1.0) {
+          state.brushLineWidth -= 1.0;
+        } else if (state.shapeLineWidth > 1.0) {
+          state.shapeLineWidth -= 1.0;
+        }
+        break;
+      case "P":
+        state.pencilSelected = true;
+        state.brushSelected = false;
+        state.rectangleSelected = false;
+        state.circleSelected = false;
+        context.lineWidth = state.pencilLineWidth;
+        console.log(`Pencil line width: ${context.lineWidth}`);
+        break;
+      case "B":
+        state.brushSelected = true;
+        state.pencilSelected = false;
+        state.rectangleSelected = false;
+        state.circleSelected = false;
+        context.lineWidth = state.brushLineWidth;
+        console.log(`Brush line width: ${context.lineWidth}`);
+        break;
+      case "R":
+        state.rectangleSelected = true;
+        state.pencilSelected = false;
+        state.brushSelected = false;
+        state.circleSelected = false;
+        context.lineWidth = state.shapeLineWidth;
+        console.log(`Shape line width: ${context.lineWidth}`);
+        break;
+      case "C":
+        state.circleSelected = true;
+        state.pencilSelected = false;
+        state.brushSelected = false;
+        state.rectangleSelected = false;
+        context.lineWidth = state.shapeLineWidth;
+        console.log(`Shape line width: ${context.lineWidth}`);
+        break;
+    }
+  };
+
+  canvas.onmousemove = (e) => {
+    state.mouseX = e.clientX;
+    state.mouseY = e.clientY;
+
+    if (state.mouseClicked) {
+      if (state.rectangleSelected) {
+        context.putImageData(state.contextImageData, 0, 0);
+
+        const rectWidth = state.mouseX - state.shapeStartX;
+        const rectHeight = state.mouseY - state.shapeStartY;
+        context.strokeRect(
+          state.shapeStartX,
+          state.shapeStartY,
+          rectWidth,
+          rectHeight
+        );
+      } else if (state.circleSelected) {
+        context.putImageData(state.contextImageData, 0, 0);
+
+        const circleRadius = Math.sqrt(
+          (state.mouseX - state.shapeStartX) *
+            (state.mouseX - state.shapeStartX) +
+            (state.mouseY - state.shapeStartY) *
+              (state.mouseY - state.shapeStartY)
+        );
+        context.beginPath();
+        context.arc(
+          state.shapeStartX,
+          state.shapeStartY,
+          circleRadius,
+          0,
+          2 * Math.PI
+        );
+        context.stroke();
+      } else {
+        // pencil or brush selected
+        context.lineTo(state.mouseX, state.mouseY);
+        context.stroke();
+      }
+    }
+  };
+
+  canvas.onmousedown = (e) => {
+    state.mouseClicked = true;
+    if (state.rectangleSelected || state.circleSelected) {
+      state.shapeStartX = e.clientX;
+      state.shapeStartY = e.clientY;
+      state.contextImageData = context.getImageData(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+    }
+    context.beginPath();
+    context.moveTo(state.mouseX, state.mouseY);
+  };
+
+  canvas.onmouseup = (e) => {
+    state.mouseClicked = false;
+    if (state.rectangleSelected) {
+      const rectWidth = state.mouseX - state.shapeStartX;
+      const rectHeight = state.mouseY - state.shapeStartY;
+
+      context.beginPath();
+      context.strokeRect(
+        state.shapeStartX,
+        state.shapeStartY,
+        rectWidth,
+        rectHeight
+      );
+    } else if (state.circleSelected) {
+      const circleRadius = Math.sqrt(
+        (state.mouseX - state.shapeStartX) *
+          (state.mouseX - state.shapeStartX) +
+          (state.mouseY - state.shapeStartY) *
+            (state.mouseY - state.shapeStartY)
+      );
+
+      context.beginPath();
+      context.arc(
+        state.shapeStartX,
+        state.shapeStartY,
+        circleRadius,
+        0,
+        2 * Math.PI
+      );
+      context.stroke();
+    }
+  };
+} else {
+  // canvas is not supported
+  const body = document.querySelector("body");
+  body.textContent = "Canvas not supported";
+  console.error("Canvas is not supported");
+}
